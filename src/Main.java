4707e078128f9024dcd1b9fa7914d123cad98d93
muhammad.khadafi <yusuf.khadafi@pusilkom.com>
2024-03-24 11:21:05 +0700

    create business logic

--- src/Main.java
+++ src/Main.java
@@ -3,4 +3,25 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    /**
+     * menampilkan todolist
+     */
+    public static void showTodoList() {
+
+    }
+
+    /**
+     * menambah todo
+     */
+    public static void addTodoList() {
+
+    }
+
+    /**
+     * menghapus todo
+     */
+    public static void removeTodoList() {
+
+    }
 }
